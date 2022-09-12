d0edf2d90abd28ddf67593f48d3332987e793c77
Lucas Coelho <ext_lucacoel@mercadolivre.com>
2022-09-12 00:11:58 -0300

    feat: updating extract transfer dto

--- src/main/kotlin/com/example/jayaBank/dtos/TransferExtractDTO.kt
+++ src/main/kotlin/com/example/jayaBank/dtos/TransferExtractDTO.kt
@@ -3,12 +3,15 @@ package com.example.jayaBank.dtos
 import java.math.BigDecimal
 import java.time.ZonedDateTime
 
-class TransferExtractDTO(
+data class TransferExtractDTO(
     val transferValue: BigDecimal,
+    val conversionRate: BigDecimal,
     val previousBalance: BigDecimal,
     val currentBalance: BigDecimal,
     val recipient: String,
     val recipientDocument: String,
+    val recipientCoin: String,
+    val originCoin: String,
     val dateOfTransaction: ZonedDateTime
 ) {
 }
