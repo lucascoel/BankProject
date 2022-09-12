22d79f2cf400b3b5bf3b67a938e0eb83da2d2884
Lucas Coelho <ext_lucacoel@mercadolivre.com>
2022-09-12 00:19:27 -0300

    feat: adding enum for coins of account

--- src/main/kotlin/com/example/jayaBank/dtos/CreateAccountDTO.kt
+++ src/main/kotlin/com/example/jayaBank/dtos/CreateAccountDTO.kt
@@ -1,8 +1,9 @@
 package com.example.jayaBank.dtos
 
-class CreateAccountDTO(
+data class CreateAccountDTO(
     val name: String,
     val document: String,
-    val password: String
+    val password: String,
+    val coin: String
 ) {
 }
