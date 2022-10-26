836022f34b00ecd99f949aba716228e808f2894c
Lucas Coelho <ext_lucacoel@mercadolivre.com>
2022-10-26 11:01:43 -0300

    fix: adding data in class

--- src/main/kotlin/com/example/jayaBank/dtos/AccountResponseDTO.kt
+++ src/main/kotlin/com/example/jayaBank/dtos/AccountResponseDTO.kt
@@ -3,10 +3,9 @@ package com.example.jayaBank.dtos
 import com.example.jayaBank.models.Coin
 import java.math.BigDecimal
 
-class AccountResponseDTO(
+data class AccountResponseDTO(
     val name: String,
     val document: String,
     val balance: BigDecimal,
     val coin: Set<Coin>
-) {
-}
+)
