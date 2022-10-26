5758910a4741cb8765f5d15554bc3b034a6ce1bf
Lucas Coelho <ext_lucacoel@mercadolivre.com>
2022-10-26 11:09:28 -0300

    feat: adding response status created

--- src/main/kotlin/com/example/jayaBank/controllers/UserController.kt
+++ src/main/kotlin/com/example/jayaBank/controllers/UserController.kt
@@ -3,11 +3,13 @@ package com.example.jayaBank.controllers
 import com.example.jayaBank.dtos.CreateAccountDTO
 import com.example.jayaBank.models.Account
 import com.example.jayaBank.services.UserService
+import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -17,6 +19,7 @@ class UserController(
 ) {
 
     @PostMapping("/createAccount")
+    @ResponseStatus(HttpStatus.CREATED)
     fun createAccount(@RequestBody account: CreateAccountDTO) = userService.createUserAccount(account)
 
     @PutMapping("/updateAccount")
