e76abf9d39e3ead0022425fccc4dd7ea084eeb21
Lucas Coelho <ext_lucacoel@mercadolivre.com>
2022-10-26 11:08:04 -0300

    fix: update type for Timestamp

--- src/main/kotlin/com/example/jayaBank/dtos/ExchangeDTO.kt
+++ src/main/kotlin/com/example/jayaBank/dtos/ExchangeDTO.kt
@@ -2,11 +2,10 @@ package com.example.jayaBank.dtos
 
 import java.math.BigDecimal
 import java.sql.Date
-import java.sql.Timestamp
 
 data class ExchangeDTO(
     val success: String,
-    val timestamp: Timestamp,
+    val timestamp: String,
     val base: String,
     val date: Date,
     val rates: Map<String,BigDecimal>
