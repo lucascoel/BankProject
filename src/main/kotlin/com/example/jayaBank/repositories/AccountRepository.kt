a6739a3d66a0bbcf46466e0073915f195d947a34
Lucas Coelho <ext_lucacoel@mercadolivre.com>
2022-10-26 11:01:07 -0300

    fix: remove imports

--- src/main/kotlin/com/example/jayaBank/repositories/AccountRepository.kt
+++ src/main/kotlin/com/example/jayaBank/repositories/AccountRepository.kt
@@ -3,7 +3,6 @@ package com.example.jayaBank.repositories
 import com.example.jayaBank.models.Account
 import org.springframework.data.mongodb.repository.MongoRepository
 import org.springframework.stereotype.Repository
-import java.util.*
 
 @Repository
 interface AccountRepository: MongoRepository<Account, String>{
