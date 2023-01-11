001bc7c704dad59c7f20800668f5344a65248dc5
Lucas Coelho <ext_lucacoel@mercadolivre.com>
2023-01-11 09:27:06 -0300

    feat: added new find user auth

--- src/main/kotlin/com/example/jayaBank/repositories/AccountRepository.kt
+++ src/main/kotlin/com/example/jayaBank/repositories/AccountRepository.kt
@@ -3,9 +3,11 @@ package com.example.jayaBank.repositories
 import com.example.jayaBank.models.Account
 import org.springframework.data.mongodb.repository.MongoRepository
 import org.springframework.stereotype.Repository
+import java.util.*
 
 @Repository
 interface AccountRepository: MongoRepository<Account, String>{
     fun findBydocument(document: String): Account?
+    fun findByUserAuthenticated(id: String): Account
 
 }
