2fd98b806640d2b52642e5c7df3d454207b1a4f9
Lucas Coelho <ext_lucacoel@mercadolivre.com>
2023-01-11 09:26:34 -0300

    feat: update controller and remove @valid

--- src/main/kotlin/com/example/jayaBank/controllers/AccountController.kt
+++ src/main/kotlin/com/example/jayaBank/controllers/AccountController.kt
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import java.math.BigDecimal
+import javax.validation.Valid
 
 @RestController
 @RequestMapping("/account")
@@ -28,7 +29,7 @@ class AccountController(
     fun checkBalance() = accountService.checkBalanceInAccountUser()
 
     @PostMapping("/transfer")
-    fun transferenciaDeSaldo(@RequestBody transferDTO: TransferDTO): TransferExtractDTO {
+    fun transferBalance(@RequestBody transferDTO: TransferDTO): TransferExtractDTO {
         return accountService.transferBalanceBetweenAccounts(transferDTO)
     }
 }
